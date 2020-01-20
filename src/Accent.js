7d14cd038177064aa7bc99905acaf21b05d48b14
will.crossland <will.crossland01@bbc.co.uk>
2020-01-20 22:19:38 +0000

    Focused input yellow. IOS inputs behave rationally

--- src/Accent.js
+++ src/Accent.js
@@ -9,21 +9,31 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   edit: e => dispatch(editAccent(e.target.value)),
-  reset: () => dispatch(resetAccent()),
-  update: e => e.keyCode === 13 && dispatch(updateAccent(e.target.value))
+  keyUp: e => {
+    if (e.keyCode === 13) { // ENTER
+      e.preventDefault()
+      dispatch(updateAccent(e.target.value))
+    } else if (e.keyCode === 27) { // ESC
+      e.preventDefault()
+      dispatch(resetAccent())
+    }
+  },
+  update: e => dispatch(updateAccent(e.target.value))
 })
 
-const Accent = ({ accent, edit, reset, uncommited, update }) => (
+const Accent = ({ accent, edit, keyUp, uncommited, update }) => (
   <div class="numeric-input__wrapper">
     <input
       id="accent"
       class={`numeric-input ${uncommited ? ' numeric-input--uncommited' : ''}`}
       type="number"
+      inputMode="numeric"
       value={accent}
       name="accent"
-      onKeyUp={update}
+      onKeyUp={keyUp}
       onChange={edit}
-      onBlur={reset}
+      onBlur={update}
+      onFocus={() => document.getElementById('accent').select()}
     />
     <label for="accent" class="numeric-input__label">
       <span>Accent</span>
--- src/BPM.js
+++ src/BPM.js
@@ -9,21 +9,31 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   edit: e => dispatch(editBpm(e.target.value)),
-  reset: () => dispatch(resetBpm()),
-  update: e => e.keyCode === 13 && dispatch(updateBpm(e.target.value))
+  keyUp: e => {
+    if (e.keyCode === 13) { // ENTER
+      e.preventDefault()
+      dispatch(updateBpm(e.target.value))
+    } else if (e.keyCode === 27) { // ESC
+      e.preventDefault()
+      dispatch(resetBpm())
+    }
+  },
+  update: e => dispatch(updateBpm(e.target.value))
 })
 
-const BPM = ({ bpm, edit, reset, uncommited, update }) => (
+const BPM = ({ bpm, edit, keyUp, uncommited, update }) => (
   <div class="numeric-input__wrapper">
     <input
       id="bpm"
       class={`numeric-input ${uncommited ? ' numeric-input--uncommited' : ''}`}
       type="number"
+      inputMode="numeric"
       name="bpm"
       value={bpm}
-      onKeyUp={update}
+      onKeyUp={keyUp}
       onChange={edit}
-      onBlur={reset}
+      onBlur={update}
+      onFocus={() => document.getElementById('bpm').select()}
     />
     <label for="bpm" class="numeric-input__label">
       <span>BPM</span>
